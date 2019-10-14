2e0c470be7a5e19fcb7faaf7bd198ef75c2bc3d0
Osaca13 <odlyss@gmail.com>
2019-10-14 11:00:15 +0200

    servicio

--- HMWeb.Core/Servicios/MaquinariasServicio.cs
+++ HMWeb.Core/Servicios/MaquinariasServicio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HMWeb.Biblioteca.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace HMWeb.Core.Servicios
 {
     public class MaquinariasServicio
     {
+        private readonly HMContext _context;
+
+        public MaquinariasServicio(HMContext context)
+        {
+            _context = context;
+        }
     }
 }
